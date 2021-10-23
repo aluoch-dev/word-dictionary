5838ff1b081c7d90d2e10730050b029f42752010
Laurine Adhiambo <laurinealuoch7@gmail.com>
2021-10-23 22:23:55 +0300

    Raywenderlich: words-dictionary

---
+++ app/build/tmp/kapt3/stubs/debug/com/raywenderlich/android/words/WordsApp.java
@@ -0,0 +1,11 @@
+package com.raywenderlich.android.words;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 5, 1}, k = 1, d1 = {"\u0000\f\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002\u00a8\u0006\u0003"}, d2 = {"Lcom/raywenderlich/android/words/WordsApp;", "Landroid/app/Application;", "()V", "app_debug"})
+public final class WordsApp extends android.app.Application {
+    
+    public WordsApp() {
+        super();
+    }
+}
